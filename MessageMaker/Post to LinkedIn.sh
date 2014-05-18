d604b379b633281b6f42bcaef08b819ac9382ff4
Evan Lovely <elovely@phase2technology.com>
2014-05-17 19:21:28 -0700

    All 4 Native OS X Social Posters: Facebook, Twitter, LinkedIn, and iMessages. Thanks to PopClip.

---
+++ MessageMaker/Post to LinkedIn.sh
@@ -0,0 +1,5 @@
+# Uses OS X Native Interface
+
+./_MessageMaker com.apple.share.LinkedIn.post text "$(pbpaste)" > /dev/null &
+
+# Thanks PopClip!
---
+++ MessageMaker/Post to Tweet.sh
@@ -0,0 +1,5 @@
+# Uses OS X Native Interface
+
+./_MessageMaker com.apple.share.Twitter.post text "$(pbpaste)" > /dev/null &
+
+# Thanks PopClip!
