2dc1c42b87a9740210e09865581f06911cdad18e
Evan Lovely <elovely@phase2technology.com>
2014-05-24 10:12:58 -0700

    Fixed Append to List

--- Append to Idea List.sh
+++ Append to Idea List.sh
@@ -1,3 +1,8 @@
 # Add to bottom of notes/ideas.md
-echo "- Date: $(date +%F)\n \n$(pbpaste)\n \n --- \n" >> ~/Dropbox/library/notes/ideas.md
+echo "- Date: $(date +%F)
+
+$(pbpaste)
+
+---
+" >> ~/Dropbox/library/notes/ideas.md
 echo "Added to Idea List"
